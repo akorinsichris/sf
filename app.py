f29f68169a2ff75ec303bbc971408cb3915aa946
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-07-04 20:50:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-st.write(### 'Hello Chris')
+import streamlit as st
+import snowflake.snowpark import Session
+
+st.title('Streamlit to Snowflake')
+
+# established snowflake connection
+@st.cache_resource
+def create_session():
+  return Session.builder.configs(st.secrets.snowflake).create()
+
+session = create_session()
+st.success("Connected to Snowflake!")
